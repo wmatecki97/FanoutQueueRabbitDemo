1eef2c57869f6dad0904f9716f3a9147ffa8e7ad
wmate <wmate@WIKTOR-YOGA>
2022-09-10 20:48:55 +0200

    Add project files.

---
+++ FanoutConsumer/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+Console.WriteLine("Hello, World!");
+var factory = new ConnectionFactory()
+{
+    HostName = "localhost",
+    Port = 5672,
+    UserName = "guest",
+    Password = "guest"
+};
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+var consumet = new EventingBasicConsumer(channel);
+consumet.Received += Consumer_Revieved;
+
+//channel.QueueDeclare("test",true,autoDelete:false);
+channel.BasicConsume("test", true, consumet);
+
+Console.ReadLine();
+
+void Consumer_Revieved(object? sender, BasicDeliverEventArgs e)
+{
+    string message = Encoding.UTF8.GetString(e.Body.ToArray());
+    Console.WriteLine(message);
+
+}
---
+++ FanoutQueueRabbit/Program.cs
@@ -0,0 +1,63 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+IConnection conn;
+IModel channel;
+
+ConnectionFactory factory = new ConnectionFactory();
+factory.HostName = "localhost";
+factory.VirtualHost = "/";
+factory.Port = 5672;
+factory.UserName = "guest";
+factory.Password = "guest";
+
+conn = factory.CreateConnection();
+channel = conn.CreateModel();
+
+channel.ExchangeDeclare(
+    "ex.fanout",
+    ExchangeType.Fanout,
+    true,
+    false,
+    null);
+
+channel.QueueDeclare(
+    "test",
+    true,
+    false,
+    false,
+    null);
+
+channel.QueueDeclare(
+    "my.queue2",
+    true,
+    false,
+    false,
+    null);
+
+channel.QueueBind("test", "ex.fanout", "");
+channel.QueueBind("my.queue2", "ex.fanout", "");
+
+channel.BasicPublish(
+    "ex.fanout",
+    "",
+    null,
+    Encoding.UTF8.GetBytes("Message 1")
+    );
+
+channel.BasicPublish(
+    "ex.fanout",
+    "",
+    null,
+    Encoding.UTF8.GetBytes("Message 2")
+    );
+
+Console.WriteLine("Press a key to exit.");
+Console.ReadKey();
+
+channel.QueueDelete("my.queue1");
+channel.QueueDelete("my.queue2");
+channel.ExchangeDelete("ex.fanout");
+
+channel.Close();
+conn.Close();
